f12067f477e98c624da62527adbe4ccc9a39a21f
palladin <nick.palladinos@gmail.com>
2020-10-27 19:28:36 +0200

    Init experiment.

---
+++ SqlDsl.ConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using SqlDsl.Core;
+
+namespace SqlDsl.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var add = new SqlIntAdd(new SqlIntValue(1), new SqlIntValue(1));
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
