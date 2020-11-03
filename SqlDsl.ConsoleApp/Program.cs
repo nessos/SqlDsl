917622736cd872441bd56fb73556e67b9d232d46
palladin <nick.palladinos@gmail.com>
2020-11-03 19:17:48 +0200

    Class 2

--- SqlDsl.ConsoleApp/Program.cs
+++ SqlDsl.ConsoleApp/Program.cs
@@ -7,9 +7,15 @@ namespace SqlDsl.ConsoleApp
     {
         static void Main(string[] args)
         {
-            var add = new SqlIntAdd(new SqlIntValue(1), new SqlIntValue(1));
+            SqlExpr<SqlInt> first = new SqlIntValue(1);
+            SqlExpr<SqlInt> second = new SqlIntValue(3);
+            var add = new SqlIntAdd(first, new SqlIntMult(first, second));
 
-            Console.WriteLine("Hello World!");
+
+            
+            
+
+            Console.WriteLine(SqlCompiler.CompileExpr(add));
         }
     }
 }
---
+++ src/SqlDsl.Core/SqlExpr.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SqlDsl.Core
+{
+    public interface SqlExpr { }
+    
+    public interface SqlExpr<TSqlType> : SqlExpr where TSqlType : SqlType { }
+
+    public interface SqlBinExpr<TSqlType> : SqlExpr<TSqlType> where TSqlType : SqlType
+    {
+        SqlExpr<TSqlType> Left { get; }
+        SqlExpr<TSqlType> Right { get; }
+    }
+
+    public class SqlIntAdd : SqlBinExpr<SqlInt>
+    {
+        public SqlExpr<SqlInt> Left { get; }
+        public SqlExpr<SqlInt> Right { get; }
+        public SqlIntAdd(SqlExpr<SqlInt> left, SqlExpr<SqlInt> right)
+        {
+            this.Left = left;
+            this.Right = right;
+        }
+        public void Deconstruct(out SqlExpr<SqlInt> left, out SqlExpr<SqlInt> right)
+        {
+            left = this.Left;
+            right = this.Right;
+        }
+    }
+
+    public class SqlIntMult : SqlBinExpr<SqlInt>
+    {
+        public SqlExpr<SqlInt> Left { get; }
+        public SqlExpr<SqlInt> Right { get; }
+        public SqlIntMult(SqlExpr<SqlInt> left, SqlExpr<SqlInt> right)
+        {
+            this.Left = left;
+            this.Right = right;
+        }
+        public void Deconstruct(out SqlExpr<SqlInt> left, out SqlExpr<SqlInt> right)
+        {
+            left = this.Left;
+            right = this.Right;
+        }
+    }
+
+    public class SqlIntValue : SqlExpr<SqlInt>
+    {
+        public int Value { get; private set; }
+        public SqlIntValue(int v)
+        {
+            this.Value = v;
+        }
+        public void Deconstruct(out int value)
+        {
+            value = this.Value;
+        }
+    }
+
+    public class SqlStringConcat : SqlBinExpr<SqlString>
+    {
+        public SqlExpr<SqlString> Left { get; }
+        public SqlExpr<SqlString> Right { get; }
+        public SqlStringConcat(SqlExpr<SqlString> left, SqlExpr<SqlString> right)
+        {
+            this.Left = left;
+            this.Right = right;
+        }
+    }
+
+}
