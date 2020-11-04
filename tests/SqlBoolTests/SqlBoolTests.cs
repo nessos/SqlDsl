467f520786a231a3c7890f8befff364c423de5e0
Alexandros Kritikos <akritikos@outlook.com>
2020-11-04 15:13:03 +0200

    Adds basic tests for sql bool types and operations

---
+++ tests/SqlBoolTests/SqlBoolTests.cs
@@ -0,0 +1,48 @@
+using SqlDsl.Core;
+using Xunit;
+
+namespace SqlBoolTests
+{
+	public class SqlBoolTests
+	{
+		public static SqlExpr<SqlBool> SqlTrue = new SqlBoolValue(true);
+		public static SqlExpr<SqlBool> SqlFalse = new SqlBoolValue(false);
+
+		[Fact]
+		public void SqlBoolValue()
+		{
+			var sql = SqlTrue.CompileExpr();
+			Assert.Equal("TRUE", sql);
+
+			sql = SqlFalse.CompileExpr();
+			Assert.Equal("FALSE", sql);
+		}
+
+		[Fact]
+		public void SqlBoolAnd()
+		{
+			var and = new SqlBoolAnd(SqlTrue, SqlFalse);
+			var sql = and.CompileExpr();
+
+			Assert.Equal("(TRUE AND FALSE)", sql);
+		}
+
+		[Fact]
+		public void SqlBoolOr()
+		{
+			var and = new SqlBoolOr(SqlTrue, SqlFalse);
+			var sql = and.CompileExpr();
+
+			Assert.Equal("(TRUE OR FALSE)", sql);
+		}
+
+		[Fact]
+		public void SqlNotTest()
+		{
+			var notSql = new SqlBoolNot(SqlFalse);
+			var sql = notSql.CompileExpr();
+
+			Assert.Equal("NOT (FALSE)", sql);
+		}
+	}
+}
