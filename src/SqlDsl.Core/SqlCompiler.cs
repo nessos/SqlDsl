9bfd979c303409c59b3b0b11d54dd9c6a868c8c3
Alexandros Kritikos <akritikos@outlook.com>
2020-11-04 15:08:26 +0200

    Compiles bool operations and values

--- src/SqlDsl.Core/SqlCompiler.cs
+++ src/SqlDsl.Core/SqlCompiler.cs
@@ -1,18 +1,26 @@
 using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace SqlDsl.Core
 {
-    public static class SqlCompiler
-    {
-        public static string CompileExpr(this SqlExpr expr) =>
-            expr switch
-            {
-                SqlIntValue(var value) => value.ToString(),
-                SqlIntAdd(var left, var right) => $"({CompileExpr(left)} + {CompileExpr(right)})",
-                SqlIntMult(var left, var right) => $"({CompileExpr(left)} * {CompileExpr(right)})",
-                _ => throw new Exception($"Not supported {expr}")
-            };
-    }
+	public static class SqlCompiler
+	{
+		public static string CompileExpr(this SqlExpr expr) =>
+			expr switch
+			{
+				// Values
+				SqlIntValue(var value) => value.ToString(),
+				SqlBoolValue(var value) => value.ToString().ToUpper(),
+
+				// Expressions - Boolean
+				SqlBoolNot(var value) => $"NOT ({CompileExpr(value)})",
+				SqlBoolAnd(var left, var right) => $"({CompileExpr(left)} AND {CompileExpr(right)})",
+				SqlBoolOr(var left, var right) => $"({CompileExpr(left)} OR {CompileExpr(right)})",
+
+				// Expressions - Numeric
+				SqlIntAdd(var left, var right) => $"({CompileExpr(left)} + {CompileExpr(right)})",
+				SqlIntMult(var left, var right) => $"({CompileExpr(left)} * {CompileExpr(right)})",
+
+				_ => throw new Exception($"Not supported {expr}")
+			};
+	}
 }
