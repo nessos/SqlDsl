d264fbfff81220b58403cf790040a76a514cacc3
albenarammou <albenarammou@gmail.com>
2020-11-10 11:06:22 +0200

    SqlStringExpr

--- src/SqlDsl.Core/SqlStringExpr.cs
+++ src/SqlDsl.Core/SqlStringExpr.cs
@@ -1,6 +1,19 @@
 namespace SqlDsl.Core
 {
 
+    public class SqlStringValue : SqlExpr<SqlString>
+    {
+        public string Value { get; private set; }
+        public SqlStringValue(string v)
+        {
+            this.Value = v;
+        }
+        public void Deconstruct(out string value)
+        {
+            value = this.Value;
+        }
+    }
+
     public class SqlStringConcat : SqlBinExpr<SqlString>
     {
         public SqlExpr<SqlString> Left { get; }
@@ -12,5 +25,32 @@ namespace SqlDsl.Core
         }
     }
 
+    public class SqlStringToUpper : SqlExpr<SqlString>
+    {
+        public SqlExpr<SqlString> Value { get; }
+        public SqlStringToUpper(SqlExpr<SqlString> value)
+        {
+            this.Value = value;
+        }
+
+        public void Deconstruct(out SqlExpr<SqlString> value)
+        {
+            value = this.Value;
+        }
+    }
+
+    public class SqlStringToLower : SqlExpr<SqlString>
+    {
+        public SqlExpr<SqlString> Value { get; }
+        public SqlStringToLower(SqlExpr<SqlString> value)
+        {
+            this.Value = value;
+        }
+
+        public void Deconstruct(out SqlExpr<SqlString> value)
+        {
+            value = this.Value;
+        }
+    }
 
 }
