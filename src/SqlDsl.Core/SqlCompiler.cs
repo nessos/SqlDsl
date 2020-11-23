438ac042d10b867f3379fb95eba2d6d84adb10c0
Alexandros Kritikos <akritikos@outlook.com>
2020-11-23 10:52:59 +0200

    Fixes indentation

--- src/SqlDsl.Core/SqlCompiler.cs
+++ src/SqlDsl.Core/SqlCompiler.cs
@@ -4,22 +4,22 @@ namespace SqlDsl.Core
 {
 	public static class SqlCompiler
 	{
-
 		private static SqlExpr OptimizeExpr(SqlExpr expr) =>
 			expr switch
 			{
 				SqlIntAdd(SqlIntValue(var left), SqlIntValue(var right)) => new SqlIntValue(left + right),
 				SqlIntAdd(var left, SqlIntValue(0)) => OptimizeExpr(left),
 				SqlIntAdd(SqlIntValue(0), var right) => OptimizeExpr(right),
-				SqlIntAdd(var left, var right) => new SqlIntAdd(OptimizeExpr(left) as SqlExprInt, OptimizeExpr(right) as SqlExprInt),
+				SqlIntAdd(var left, var right) => new SqlIntAdd(OptimizeExpr(left) as SqlExprInt,
+					OptimizeExpr(right) as SqlExprInt),
 				_ => expr
 			};
 
 		public static SqlExpr MultiOptimizer(SqlExpr expr)
 		{
 			var optExpr = OptimizeExpr(expr);
-			return expr == optExpr 
-				? optExpr 
+			return expr == optExpr
+				? optExpr
 				: MultiOptimizer(optExpr);
 		}
 
@@ -35,10 +35,10 @@ namespace SqlDsl.Core
 				SqlBoolAnd(var left, var right) => $"({EmitExpr(left)} AND {EmitExpr(right)})",
 				SqlBoolOr(var left, var right) => $"({EmitExpr(left)} OR {EmitExpr(right)})",
 
-        SqlStringValue(var value) => $"'{value}'",
+				SqlStringValue(var value) => $"'{value}'",
 				SqlStringToUpper(var value) => $"{EmitExpr(value).ToUpper()}",
-				SqlStringToLower(var value) => $"{EmitExpr(value).ToLower()}" ,
-          
+				SqlStringToLower(var value) => $"{EmitExpr(value).ToLower()}",
+
 				// Expressions - Numeric
 				SqlIntAdd(var left, var right) => $"({CompileExpr(left)} + {CompileExpr(right)})",
 				SqlIntSub(var left, var right) => $"({CompileExpr(left)} - {CompileExpr(right)})",
@@ -46,9 +46,10 @@ namespace SqlDsl.Core
 				SqlIntPlus(var value) => $"({CompileExpr(value)})",
 				SqlIntMinus(var value) => $"(-({CompileExpr(value)}))",
 				SqlIntAbs(var value) => $"(ABS({CompileExpr(value)}))",
-          
+
 				_ => throw new Exception($"Not supported {expr}")
 			};
+
 		public static string CompileExpr(this SqlExpr expr) => EmitExpr(MultiOptimizer(expr));
 	}
 }
