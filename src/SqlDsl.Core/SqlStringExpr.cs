b7cf217285a1c9844270ddef51eca473762f28f6
Alexandros Kritikos <akritikos@outlook.com>
2020-11-24 15:12:48 +0200

    Turns string expressions to records

--- src/SqlDsl.Core/SqlStringExpr.cs
+++ src/SqlDsl.Core/SqlStringExpr.cs
@@ -1,65 +1,14 @@
 namespace SqlDsl.Core
 {
-	public class SqlStringValue : SqlExpr<SqlString>
-	{
-		public string Value { get; private set; }
+	public record SqlStringValue(string Value)
+		: SqlExprString;
 
-		public SqlStringValue(string v)
-		{
-			this.Value = v;
-		}
+	public record SqlStringConcat(SqlExpr<SqlString> Left, SqlExpr<SqlString> Right)
+		: SqlExprString, SqlBinExpr<SqlString>;
 
-		public void Deconstruct(out string value)
-		{
-			value = this.Value;
-		}
-	}
+	public record SqlStringToUpper(SqlExpr<SqlString> Value)
+		: SqlExprString, SqlUnaryExpr<SqlString>;
 
-	public class SqlStringConcat : SqlBinExpr<SqlString>
-	{
-		public SqlExpr<SqlString> Left { get; }
-		public SqlExpr<SqlString> Right { get; }
-
-		public SqlStringConcat(SqlExpr<SqlString> left, SqlExpr<SqlString> right)
-		{
-			this.Left = left;
-			this.Right = right;
-		}
-
-		public void Deconstruct(out SqlExpr<SqlString> left, out SqlExpr<SqlString> right)
-		{
-			left = this.Left;
-			right = this.Right;
-		}
-	}
-
-	public class SqlStringToUpper : SqlExpr<SqlString>
-	{
-		public SqlExpr<SqlString> Value { get; }
-
-		public SqlStringToUpper(SqlExpr<SqlString> value)
-		{
-			this.Value = value;
-		}
-
-		public void Deconstruct(out SqlExpr<SqlString> value)
-		{
-			value = this.Value;
-		}
-	}
-
-	public class SqlStringToLower : SqlExpr<SqlString>
-	{
-		public SqlExpr<SqlString> Value { get; }
-
-		public SqlStringToLower(SqlExpr<SqlString> value)
-		{
-			this.Value = value;
-		}
-
-		public void Deconstruct(out SqlExpr<SqlString> value)
-		{
-			value = this.Value;
-		}
-	}
+	public record SqlStringToLower(SqlExpr<SqlString> Value)
+		: SqlExprString, SqlUnaryExpr<SqlString>;
 }
